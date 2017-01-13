3af5757fbc526c1452307617e8fabb11bdbd31a3
Gayed, Jeremy <jeremy.gayed@nytimes.com>
2017-01-13 00:28:47 -0500

    Working, but tests are weird :/

--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
 export { default as withTracking } from './withTrackingComponentDecorator';
 export { default as trackEvent } from './trackEventMethodDecorator';
+export { default } from './trackingHoC';
