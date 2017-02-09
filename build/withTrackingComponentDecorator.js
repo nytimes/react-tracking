6ef98ae3b5c997f4f28e19dd964f12c9bdfce4c6
Gayed, Jeremy <jeremy.gayed@nytimes.com>
2017-02-09 14:20:11 -0500

    build

--- build/withTrackingComponentDecorator.js
+++ build/withTrackingComponentDecorator.js
@@ -40,8 +40,14 @@ var _dispatchTrackingEvent2 = _interopRequireDefault(_dispatchTrackingEvent);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+var TrackingPropType = _react.PropTypes.shape({
+  data: _react.PropTypes.object,
+  dispatch: _react.PropTypes.func
+});
+
 function withTrackingComponentDecorator() {
-  var trackingContext = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
+  var trackingData = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
+  var options = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : { dispatch: _dispatchTrackingEvent2.default };
 
   return function (DecoratedComponent) {
     var _class, _temp2;
@@ -63,22 +69,33 @@ function withTrackingComponentDecorator() {
         }
 
         return _ret = (_temp = (_this = (0, _possibleConstructorReturn3.default)(this, (_ref = WithTracking.__proto__ || (0, _getPrototypeOf2.default)(WithTracking)).call.apply(_ref, [this].concat(args))), _this), _this.trackEvent = function (data) {
-          (0, _dispatchTrackingEvent2.default)((0, _extends3.default)({}, _this.getChildContext().tracking, data));
+          _this.getTrackingDispatcher()((0, _extends3.default)({}, _this.getChildContext().tracking.data, data));
         }, _temp), (0, _possibleConstructorReturn3.default)(_this, _ret);
       }
 
       (0, _createClass3.default)(WithTracking, [{
+        key: 'getTrackingDispatcher',
+        value: function getTrackingDispatcher() {
+          return this.context.tracking && this.context.tracking.dispatch || options.dispatch;
+        }
+      }, {
         key: 'getChildContext',
         value: function getChildContext() {
-          var thisTrackingContext = typeof trackingContext === 'function' ? trackingContext(this.props) : trackingContext;
+          var thisTrackingData = typeof trackingData === 'function' ? trackingData(this.props) : trackingData;
+
+          var contextData = this.context.tracking && this.context.tracking.data || {};
+
           return {
-            tracking: (0, _extends3.default)({}, this.context.tracking, thisTrackingContext)
+            tracking: {
+              data: (0, _extends3.default)({}, contextData, thisTrackingData),
+              dispatch: this.getTrackingDispatcher()
+            }
           };
         }
       }, {
         key: 'componentDidMount',
         value: function componentDidMount() {
-          if (trackingContext.page) {
+          if (trackingData.page) {
             this.trackEvent({
               action: 'pageview'
             });
@@ -94,9 +111,9 @@ function withTrackingComponentDecorator() {
       }]);
       return WithTracking;
     }(_react.Component), _class.displayName = 'WithTracking(' + decoratedComponentName + ')', _class.contextTypes = {
-      tracking: _react.PropTypes.object
+      tracking: TrackingPropType
     }, _class.childContextTypes = {
-      tracking: _react.PropTypes.object
+      tracking: TrackingPropType
     }, _temp2;
   };
 }
