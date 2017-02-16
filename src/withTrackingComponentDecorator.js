aa3548aaf91a93126f98856a9ae89c12cf7c7fdc
Aneudy Abreu <aneudy.abreu@nytimes.com>
2017-02-16 10:54:17 -0500

    fixing merge conflicts

--- src/withTrackingComponentDecorator.js
+++ src/withTrackingComponentDecorator.js
@@ -57,7 +57,7 @@ export default function withTrackingComponentDecorator(
         }
 
         if (typeof dispatchOnMount === 'function') {
-          this.trackEvent()(dispatchOnMount(this.getTrackingData()));
+          this.trackEvent(dispatchOnMount(this.getTrackingData()));
         }
       }
 
