e683011e6a09b84d6bd894e7081f28ee744334d3
Jeremy Gayed <jeremy.gayed@nytimes.com>
2017-05-16 14:42:03 -0400

    Remove build

--- build/dispatchTrackingEvent.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.default = dispatchTrackingEvent;
-function dispatchTrackingEvent(data) {
-  (window.dataLayer = window.dataLayer || []).push(data);
-}
