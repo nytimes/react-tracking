fb14a3821875c89e3f8181fe831c44493ace6bef
Tyler Fry <fry.tyler@gmail.com>
2017-10-10 09:16:01 -0400

    Add prettier, match to eslint rules and add precommit hook

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
-export { default as withTracking, TrackingContextType } from './withTrackingComponentDecorator';
+export {
+  default as withTracking,
+  TrackingContextType,
+} from './withTrackingComponentDecorator';
 export { default as trackEvent } from './trackEventMethodDecorator';
 export { default as TrackingPropType } from './TrackingPropType';
 export { default } from './trackingHoC';
