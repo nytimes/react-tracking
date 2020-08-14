b9728a7e72ea18bd906796a4b775448da1144053
Adam Schønemann <adamschoenemann@gmail.com>
2020-08-14 13:41:55 +0200

    Add extra props argument to default dispatcher

--- src/dispatchTrackingEvent.js
+++ src/dispatchTrackingEvent.js
@@ -1,4 +1,4 @@
-export default function dispatchTrackingEvent(data) {
+export default function dispatchTrackingEvent(data, props) {
   if (Object.keys(data).length > 0) {
     (window.dataLayer = window.dataLayer || []).push(data);
   }
